cba9698ba181d36e62259807ced6abff2e70fbd1
Warunee Dinunzio <dinunziow@gmail.com>
2021-08-26 22:04:53 -0400

    create react app

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const groceries = [
+    {
+        "item": "milk",
+        "brand": "Organic valley",
+        "units": "1 gallon",
+        "quantity": 1,
+        "isPurchased": false
+    },
+    {
+        "item": "egg",
+        "brand": "Eggland's best",
+        "units": "12 ct",
+        "quantity": 1,
+        "isPurchased": false
+    },
+    {
+        "item": "chicken",
+        "brand": "Perdue",
+        "units": "1 lb",
+        "quantity": 5,
+        "isPurchased": false
+    }
+]
+export default groceries
